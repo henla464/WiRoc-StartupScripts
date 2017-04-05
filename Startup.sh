1ea49dbfe4e96b97c3b28a14bd74f2deb913eb0d
Henrik Larsson <laselase@gmail.com>
2017-04-05 19:30:26 +0200

    added modemmanager exlude rule

--- Startup.sh
+++ Startup.sh
@@ -5,7 +5,7 @@ systemctl stop bluetooth
 systemctl disable bluetooth
 hciconfig hci0 up
 /home/chip/WiRoc-StartupScripts/setGPIOuart2
-#systemctl stop getty@ttyGS0.service
+systemctl stop serial-getty@ttyGS0.service
 #(cd /home/chip/WiRoc-Python-2 && exec python3 start.py)
 #node /home/chip/WiRoc-BLE-Device/main.js &> /home/chip/WiRoc-BLE-Device.log &
 echo "End Startup.sh" >> /home/chip/startup.log
