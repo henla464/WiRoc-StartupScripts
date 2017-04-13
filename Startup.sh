bc943d063be93e045014f4d3e13ddcb7e600b7ea
Henrik Larsson <laselase@gmail.com>
2017-04-13 20:19:50 +0200

    disable getty, cleanup

--- Startup.sh
+++ Startup.sh
@@ -6,6 +6,4 @@ systemctl disable bluetooth
 hciconfig hci0 up
 /home/chip/WiRoc-StartupScripts/setGPIOuart2
 systemctl disable serial-getty@ttyGS0.service
-#(cd /home/chip/WiRoc-Python-2 && exec python3 start.py)
-#node /home/chip/WiRoc-BLE-Device/main.js &> /home/chip/WiRoc-BLE-Device.log &
 echo "End Startup.sh" >> /home/chip/startup.log
