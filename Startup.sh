59d31d2cc39208daa6dc439a35b6c2a8da67fd23
Henrik Larsson <laselase@gmail.com>
2018-09-08 15:35:38 +0200

    enable battery monitoring on nanopi and add bluetooth config script

--- Startup.sh
+++ Startup.sh
@@ -4,7 +4,12 @@ echo "Beginning Startup.sh" >> /home/chip/startup.log
 systemctl stop bluetooth
 systemctl disable bluetooth
 hciconfig hci0 up
-/home/chip/WiRoc-StartupScripts/setGPIOuart2
+if [[ $(hostname -s) = nanopiair ]]; then
+    # Enable battery monitoring, percentage calculation; (Battery current ADC Enable)
+    sudo i2cset -y -f 0 0x34 0x82 0xC3﻿﻿
+else
+    /home/chip/WiRoc-StartupScripts/setGPIOuart2
+fi
 systemctl disable serial-getty@ttyGS0.service
 systemctl stop serial-getty@ttyGS0.service
 echo "End Startup.sh" >> /home/chip/startup.log
