fb72cf9341dbf2f5d0de563eafcfa2213e9b39ff
Henrik Larsson <laselase@gmail.com>
2018-11-26 23:29:05 +0100

    add bluez with fix for ap6212

--- Startup.sh
+++ Startup.sh
@@ -3,10 +3,10 @@
 echo "Beginning Startup.sh" >> /home/chip/startup.log
 systemctl stop bluetooth
 systemctl disable bluetooth
-hciconfig hci0 up
+#hciconfig hci0 up
 if [[ $(hostname -s) = nanopiair ]]; then
     # Enable battery monitoring, percentage calculation; (Battery current ADC Enable)
-    sudo i2cset -y -f 0 0x34 0x82 0xC3﻿﻿
+    /usr/sbin/i2cset -y -f 0 0x34 0x82 0xC3
 else
     /home/chip/WiRoc-StartupScripts/setGPIOuart2
 fi
